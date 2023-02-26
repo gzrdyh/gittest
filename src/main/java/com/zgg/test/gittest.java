f487a10a3833b07a71af119eb271e45d85b91948
zgg <zgg221224@163.com>
2023-02-26 22:11:19 +0800

    third commit

--- src/main/java/com/zgg/test/gittest.java
+++ src/main/java/com/zgg/test/gittest.java
@@ -12,5 +12,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello1");
+        System.out.println("hello2");
     }
 }
