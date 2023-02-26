d82457efa082e5e364fec9642ea56393c94ebab6
zgg <zgg221224@163.com>
2023-02-27 00:09:45 +0800

    gitee first

--- src/main/java/com/zgg/test/gittest.java
+++ src/main/java/com/zgg/test/gittest.java
@@ -19,6 +19,7 @@ public class gittest {
         System.out.println("hello push");
         System.out.println("hello push1");
         System.out.println("hello pull1");
+        System.out.println("hello pushgitee");
 
 
     }
